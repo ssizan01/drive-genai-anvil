a1fd9ee1a8a23ecdce55275c95fc1510da38b0cd
showvik.sizan@gmail.com <showvik.sizan@gmail.com>
2023-10-05 16:45:33 +0000

    Created Logo-Google-Cloud.jpg; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.google.auth
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +10,10 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+
+    
+    email_addr = anvil.google.auth.login()
+    print(f"User logged in as {email_addr}")
+
