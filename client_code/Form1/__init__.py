3479731fa56ee52f3f449e34b5393f666a7c86ca
showvik.sizan@gmail.com <showvik.sizan@gmail.com>
2023-10-11 15:19:33 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -37,25 +37,27 @@ class Form1(Form1Template):
 
 
   def extract_file_content_click(self, **event_args):
-    print("extract_file_content_click triggered!")
-  
-      #self.llm_output.text = anvil.server.call('get_file_content', selected_file_id, user_query)
-    self.timer_1.interval = 5
     selected_file_id = self.dropdown_files.selected_value
     print(f"Selected File ID: {selected_file_id}")
 
     user_query= self.user_query.text
     print(f"User Query: {user_query}")
     anvil.server.call('get_file_content', selected_file_id, user_query)
+    self.timer_1.interval = 2
+
 
   def timer_1_tick(self, **event_args):
-    """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
-    print(f'tick event triggered for {self.user_email} and {self.user_query.text}')
-    # Fetch the relevant sentences from the backend using the server function
-    sentences = anvil.server.call('fetch_sentences_from_storage', self.user_email, self.dropdown_files.selected_value, self.user_query.text)
-    self.repeating_panel_1.items = sentences
-    # If you want to stop the timer after fetching the data, uncomment the next line
-    self.timer_1.interval = None
+      print('tick even triggered')
+      sentences = anvil.server.call('process_chunk', self.user_email, self.dropdown_files.selected_value, self.user_query.text)
+  
+      if not sentences:
+          print('no sentences')  # Stop the timer if no more chunks are left
+      else:
+          # Assuming that repeating_panel_1 is displaying a list of sentences:
+          current_sentences = list(self.repeating_panel_1.items)
+          current_sentences.extend(sentences)
+          self.repeating_panel_1.items = current_sentences
+
 
       
 
